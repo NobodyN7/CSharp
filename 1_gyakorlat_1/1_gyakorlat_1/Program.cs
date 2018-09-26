8fb6e6d532fc1a90cf7fe270394e079b559d7296
rublinszki <rublinszki@iit.uni-miskolc.hu>
2018-09-26 11:43:20 +0200

    elso gyak vege

--- 1_gyakorlat_1/1_gyakorlat_1/Program.cs
+++ 1_gyakorlat_1/1_gyakorlat_1/Program.cs
@@ -4,13 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _1_gyakorlat_1
+namespace autonevter.futtathato
 {
     class Program
     {
         static void Main(string[] args)
         {
+            int darab = 2;
+            Auto[] autok = new Auto[darab];
 
+            autok[0] = new Auto("Skoda", "superb", DateTime.Today.Year, "1.8");
+            Console.WriteLine(autok[0].ToString());
         }
     }
 }
